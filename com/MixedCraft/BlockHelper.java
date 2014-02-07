4b0ad3499d9599c991d4d78caa842019df81c7eb
TheSlayerMC <brock.kerley@hotmail.com>
2014-02-08 09:08:51 +1030

    started 1.7 updated

--- com/MixedCraft/BlockHelper.java
+++ com/MixedCraft/BlockHelper.java
@@ -21,67 +21,67 @@ public class BlockHelper extends HelperHandler{
 
 
 
-	public static final Block Extractor = new BlockDNAExtractor(x.ExtractorID).setUnlocalizedName("Extractor").setHardness(2.0F);
-	public static final Block Mixer = new BlockDNAMixer(x.MixerID).setUnlocalizedName("Mixer").setHardness(2.0F);
-	public static final Block Assembler = new BlockMobAssembler(x.AssemblerID).setUnlocalizedName("Assembler").setHardness(2.0F);
+	public static final Block Extractor = new BlockDNAExtractor().setBlockName("Extractor").setHardness(2.0F);
+	public static final Block Mixer = new BlockDNAMixer().setBlockName("Mixer").setHardness(2.0F);
+	public static final Block Assembler = new BlockMobAssembler().setBlockName("Assembler").setHardness(2.0F);
 
-	public static final Block CopperOre = new ModBlock(x.CopperOreID).registerTexture("CopperOre").setUnlocalizedName("CopperOre").setHardness(7.0F);
-	public static final Block BigCrafting = new BiggerCraftingTable(x.CraftingID).setUnlocalizedName("Craft").setStepSound(Block.soundWoodFootstep);
-	public static final Block TinOre = new ModBlock(x.TinOreID).registerTexture("TinOre").setUnlocalizedName("TinOre").setHardness(7.0F);
-	public static final Block CopperBlock = new ModBlock(x.CopperBlockID).registerTexture("CopperBlock").setUnlocalizedName("CopperBlock").setHardness(7.0F).setStepSound(Block.soundMetalFootstep);
-	public static final Block TinBlock = new ModBlock(x.TinBlockID).registerTexture("TinBlock").setUnlocalizedName("TinBlock").setHardness(7.0F).setHardness(7.0F).setStepSound(Block.soundMetalFootstep);	
-	public static final Block SilverBlock = new ModBlock(x.SilverBlockID).registerTexture("SilverBlock").setUnlocalizedName("SilverBlock").setHardness(7.0F).setHardness(7.0F).setStepSound(Block.soundMetalFootstep);
-	public static final Block SilverOre = new ModBlock(x.SilverOreID).registerTexture("SilverOre").setUnlocalizedName("SilverOre").setHardness(7.0F);	
-	public static final BlockSolarPanel SolarPanel = (BlockSolarPanel) new BlockSolarPanel(x.SolarPanelID).setUnlocalizedName("SolarPanel").setHardness(2.0F).setHardness(7.0F).setStepSound(Block.soundMetalFootstep);
-	public static final Block CowSheepWool = new ModBlock(x.CowSheepWoolID).registerTexture("CowSheepWool").setUnlocalizedName("CowSheepWool").setHardness(0.2F).setStepSound(Block.soundClothFootstep);
-	//public static final Block Disassembler = new Disassembler(x.DisasemblerID).setUnlocalizedName("Disasembler").setCreativeTab(MixedCraft.BlockTab);	
-	public static final Block IronFurnaceOff = (new BlockIronFurnace(x.IronFurnaceID, false)).setHardness(3.5F).setStepSound(Block.soundMetalFootstep).setUnlocalizedName("IronFurnaceOff").setCreativeTab(MixedCraft.BlockTab);
-	public static final Block IronFurnaceOn = (new BlockIronFurnace(x.IronFurnaceOnID, true)).setHardness(3.5F).setStepSound(Block.soundMetalFootstep).setUnlocalizedName("IronFurnaceOn").setLightValue(0.875F);
-	public static final Block GoldDoor = new BlockGoldDoor(x.GoldDoorID, Material.wood).setHardness(3.5F).setStepSound(Block.soundMetalFootstep).setUnlocalizedName("GoldDoor").setCreativeTab(null);
-	public static final Block Table = new BlockTable(x.TableID).setUnlocalizedName("Table").setStepSound(Block.soundWoodFootstep).setCreativeTab(MixedCraft.BlockTab);
-	public static final Block DiamondFurnaceOff = (new BlockDiamondFurnace(x.DiamondFurnaceOffID, false)).setHardness(3.5F).setStepSound(Block.soundMetalFootstep).setUnlocalizedName("DiamondFurnaceOff").setCreativeTab(MixedCraft.BlockTab);
-	public static final Block DiamondFurnaceOn = (new BlockDiamondFurnace(x.DiamondFurnaceOnID, true)).setHardness(3.5F).setStepSound(Block.soundMetalFootstep).setUnlocalizedName("DiamondFurnaceOn").setLightValue(0.875F);
+	public static final Block CopperOre = new ModBlock().registerTexture("CopperOre").setBlockName("CopperOre").setHardness(7.0F);
+	public static final Block BigCrafting = new BiggerCraftingTable().setBlockName("Craft").setStepSound(Block.soundTypeWood);
+	public static final Block TinOre = new ModBlock().registerTexture("TinOre").setBlockName("TinOre").setHardness(7.0F);
+	public static final Block CopperBlock = new ModBlock().registerTexture("CopperBlock").setBlockName("CopperBlock").setHardness(7.0F).setStepSound(Block.soundTypeMetal);
+	public static final Block TinBlock = new ModBlock().registerTexture("TinBlock").setBlockName("TinBlock").setHardness(7.0F).setHardness(7.0F).setStepSound(Block.soundTypeMetal);	
+	public static final Block SilverBlock = new ModBlock().registerTexture("SilverBlock").setBlockName("SilverBlock").setHardness(7.0F).setHardness(7.0F).setStepSound(Block.soundTypeMetal);
+	public static final Block SilverOre = new ModBlock().registerTexture("SilverOre").setBlockName("SilverOre").setHardness(7.0F);	
+	public static final BlockSolarPanel SolarPanel = (BlockSolarPanel) new BlockSolarPanel().setBlockName("SolarPanel").setHardness(2.0F).setHardness(7.0F).setStepSound(Block.soundTypeMetal);
+	public static final Block CowSheepWool = new ModBlock().registerTexture("CowSheepWool").setBlockName("CowSheepWool").setHardness(0.2F).setStepSound(Block.soundTypeCloth);
+	//public static final Block Disassembler = new Disassembler().setBlockName("Disasembler").setCreativeTab(MixedCraft.BlockTab);	
+	public static final Block IronFurnaceOff = (new BlockIronFurnace(false)).setHardness(3.5F).setStepSound(Block.soundTypeMetal).setBlockName("IronFurnaceOff").setCreativeTab(MixedCraft.BlockTab);
+	public static final Block IronFurnaceOn = (new BlockIronFurnace(true)).setHardness(3.5F).setStepSound(Block.soundTypeMetal).setBlockName("IronFurnaceOn").setLightLevel(0.875F);
+	public static final Block GoldDoor = new BlockGoldDoor(Material.wood).setHardness(3.5F).setStepSound(Block.soundTypeMetal).setBlockName("GoldDoor").setCreativeTab(null);
+	public static final Block Table = new BlockTable().setBlockName("Table").setStepSound(Block.soundTypeWood).setCreativeTab(MixedCraft.BlockTab);
+	public static final Block DiamondFurnaceOff = (new BlockDiamondFurnace(false)).setHardness(3.5F).setStepSound(Block.soundTypeMetal).setBlockName("DiamondFurnaceOff").setCreativeTab(MixedCraft.BlockTab);
+	public static final Block DiamondFurnaceOn = (new BlockDiamondFurnace(true)).setHardness(3.5F).setStepSound(Block.soundTypeMetal).setBlockName("DiamondFurnaceOn").setLightLevel(0.875F);
 
-    public static final BlockFlylightPortal FlyLightPortal = (BlockFlylightPortal) new BlockFlylightPortal(x.FlylightPortalID).registerTexture("FlyLightPortal").setUnlocalizedName("FlyLightPortal").setBlockUnbreakable();
-    public static final Block FlyLightGrass = new BlockFlyLightGrass(177).setHardness(1.5F).setLightValue(0.7F).setUnlocalizedName("FlyLightGrass").setStepSound(Block.soundGrassFootstep);
-    public static final Block FlyLightDirt = new ModDirt(178).registerTexture("FlyLightGrass_Bottom").setLightValue(0.8F).setHardness(1.0F).setUnlocalizedName("FlyLightDirt").setStepSound(Block.soundGravelFootstep);
-    public static final Block FlyLightStone = new ModBlock(179).registerTexture("FlyLightStone").setHardness(3.0F).setLightValue(0.6F).setUnlocalizedName("FlyLightStone").setStepSound(Block.soundStoneFootstep);
-    public static final Block FlyLightOre = new ModBlock(x.BlueRepeaterID).registerTexture("FlyLightOre").setLightValue(0.6F).setHardness(3.0F).setUnlocalizedName("FlyLightOre").setStepSound(Block.soundStoneFootstep);
-    public static final Block ModFire = new ModFire(x.BlueRepeaterOnID).setHardness(6.0F).setUnlocalizedName("ModFire").setStepSound(Block.soundLadderFootstep).setCreativeTab(MixedCraft.BlockTab);
+    public static final BlockFlylightPortal FlyLightPortal = (BlockFlylightPortal) new BlockFlylightPortal().registerTexture("FlyLightPortal").setBlockName("FlyLightPortal").setBlockUnbreakable();
+    public static final Block FlyLightGrass = new BlockFlyLightGrass().setHardness(1.5F).setLightLevel(0.7F).setBlockName("FlyLightGrass").setStepSound(Block.soundTypeGrass);
+    public static final Block FlyLightDirt = new ModDirt().registerTexture("FlyLightGrass_Bottom").setLightLevel(0.8F).setHardness(1.0F).setBlockName("FlyLightDirt").setStepSound(Block.soundTypeGravel);
+    public static final Block FlyLightStone = new ModBlock().registerTexture("FlyLightStone").setHardness(3.0F).setLightLevel(0.6F).setBlockName("FlyLightStone").setStepSound(Block.soundTypeStone);
+    public static final Block FlyLightOre = new ModBlock().registerTexture("FlyLightOre").setLightLevel(0.6F).setHardness(3.0F).setBlockName("FlyLightOre").setStepSound(Block.soundTypeStone);
+    public static final Block ModFire = new ModFire().setHardness(6.0F).setBlockName("ModFire").setStepSound(Block.soundTypeMetal).setCreativeTab(MixedCraft.BlockTab);
 	
-	public static final Block Test = new ModBlock(x.ColourID).registerTexture("Test").setHardness(3.5F).setStepSound(Block.soundMetalFootstep).setUnlocalizedName("Flashing").setLightValue(0.875F);
+	public static final Block Test = new ModBlock().registerTexture("Test").setHardness(3.5F).setStepSound(Block.soundTypeMetal).setBlockName("Flashing").setLightLevel(0.875F);
 
-	//public static final Block House1 = new BlockHouse1(x.House1ID).setHardness(3.5F).setStepSound(Block.soundMetalFootstep).setUnlocalizedName("House1").setLightValue(0.875F);
+	//public static final Block House1 = new BlockHouse1().setHardness(3.5F).setStepSound(Block.soundTypeMetal).setBlockName("House1").setLightLevel(0.875F);
 
-    public static final Block SlowSand = new BlockSinkSand(x.SlowSandID).setHardness(1.0F).setStepSound(Block.soundSandFootstep).setUnlocalizedName("SlowSand").setTextureName("sand");
+    public static final Block SlowSand = new BlockSinkSand().setHardness(1.0F).setStepSound(Block.soundTypeSand).setBlockName("SlowSand").setBlockTextureName("sand");
 
-    public static final Block ChocolateCake = new BlockChocolateCake(x.ChocolateCakeBlockID).setHardness(0.5F).setStepSound(Block.soundClothFootstep).setUnlocalizedName("ChocolateCake").setTextureName("ChocolateCake");
-    public static final Block ChocolateCrop = new BlockChocolateCrop(x.ChocolateCropBlockID).setUnlocalizedName("ChocolateCrop").setTextureName("ChocolateCrop");
+    public static final Block ChocolateCake = new BlockChocolateCake().setHardness(0.5F).setStepSound(Block.soundTypeCloth).setBlockName("ChocolateCake").setBlockTextureName("ChocolateCake");
+    public static final Block ChocolateCrop = new BlockChocolateCrop().setBlockName("ChocolateCrop").setBlockTextureName("ChocolateCrop");
     
-    public static final Block GoldFurnaceOff = new GoldFurnace(x.GoldFurnaceID, false).setUnlocalizedName("GoldFurnace").setCreativeTab(MixedCraft.BlockTab);
-    public static final Block GoldFurnaceOn = new GoldFurnace(x.GoldFurnaceOnID, true).setUnlocalizedName("GoldFurnace").setCreativeTab(null);
+    public static final Block GoldFurnaceOff = new GoldFurnace(false).setBlockName("GoldFurnace").setCreativeTab(MixedCraft.BlockTab);
+    public static final Block GoldFurnaceOn = new GoldFurnacex.GoldFurnaceOnID, true).setBlockName("GoldFurnace").setCreativeTab(null);
     
-    public static final Block Flower1 = new ModFlower(x.Flower1, "FlyLightFlower_1").setUnlocalizedName("Flower1").setCreativeTab(MixedCraft.BlockTab);
-    public static final Block Flower2 = new ModFlower(x.Flower2, "FlyLightFlower_2").setUnlocalizedName("Flower2").setCreativeTab(MixedCraft.BlockTab);
-    public static final Block FlylightLog = new ModLog(x.FlylightLog, "FlyLight").setUnlocalizedName("Log").setHardness(1.5F).setCreativeTab(MixedCraft.BlockTab);
-    public static final Block FlylightLeaves = new ModLeaves(x.FlylightLeaves, "FlyLight").setHardness(0.4F).setUnlocalizedName("Leaves").setCreativeTab(MixedCraft.BlockTab);
-    public static final Block FlylightFern = new ModTallGrass(x.Fern, "FlyLightFern").setUnlocalizedName("Fern").setCreativeTab(MixedCraft.BlockTab);
-    public static final Block FlylightGrass = new ModTallGrass(x.Grass, "FlyLightGrass").setUnlocalizedName("Grass").setCreativeTab(MixedCraft.BlockTab);
-    public static final Block FlylightCane = new FlylightSugarCane(x.SugarCane).registerTexture("FlyLightSugarCane").setUnlocalizedName("Cane").setCreativeTab(MixedCraft.BlockTab);
-    public static final Block FlylightSapling = new FlylightSapling(x.Sapling, "FlylightSapling").setUnlocalizedName("Sapling").setCreativeTab(MixedCraft.BlockTab);
-    public static final Block Water = new ModWater(x.ModWater).setUnlocalizedName("Water").setCreativeTab(MixedCraft.BlockTab);
-    public static final Block Vine = new ModVine(x.Vine, "Flylight").setUnlocalizedName("Vine").setCreativeTab(MixedCraft.BlockTab);
+    public static final Block Flower1 = new ModFlower("FlyLightFlower_1").setBlockName("Flower1").setCreativeTab(MixedCraft.BlockTab);
+    public static final Block Flower2 = new ModFlower("FlyLightFlower_2").setBlockName("Flower2").setCreativeTab(MixedCraft.BlockTab);
+    public static final Block FlylightLog = new ModLog("FlyLight").setBlockName("Log").setHardness(1.5F).setCreativeTab(MixedCraft.BlockTab);
+    public static final Block FlylightLeaves = new ModLeaves("FlyLight").setHardness(0.4F).setBlockName("Leaves").setCreativeTab(MixedCraft.BlockTab);
+    public static final Block FlylightFern = new ModTallGrass("FlyLightFern").setBlockName("Fern").setCreativeTab(MixedCraft.BlockTab);
+    public static final Block FlylightGrass = new ModTallGrass("FlyLightGrass").setBlockName("Grass").setCreativeTab(MixedCraft.BlockTab);
+    public static final Block FlylightCane = new FlylightSugarCane().registerTexture("FlyLightSugarCane").setBlockName("Cane").setCreativeTab(MixedCraft.BlockTab);
+    public static final Block FlylightSapling = new FlylightSapling("FlylightSapling").setBlockName("Sapling").setCreativeTab(MixedCraft.BlockTab);
+    public static final Block Water = new ModWater().setBlockName("Water").setCreativeTab(MixedCraft.BlockTab);
+    public static final Block Vine = new ModVine("Flylight").setBlockName("Vine").setCreativeTab(MixedCraft.BlockTab);
     
-    public static final Block Iron = new ModOre(x.Iron).registerTexture("Iron").setHardness(3.0F).setUnlocalizedName("Iron");
-    public static final Block Gold = new ModOre(x.Gold).registerTexture("Gold").setHardness(3.0F).setUnlocalizedName("Gold");
-    public static final Block Lapis = new ModOre(x.Lapis).registerTexture("Lapis").setHardness(3.0F).setUnlocalizedName("Lapis");
-    public static final Block Redstone = new ModOre(x.Redstone).registerTexture("Redstone").setHardness(3.0F).setUnlocalizedName("Redstone");
-    public static final Block Diamond = new ModOre(x.Diamond).registerTexture("Diamond").setHardness(3.0F).setUnlocalizedName("Diamond");
-    public static final Block Coal = new ModOre(x.Coal).registerTexture("Coal").setHardness(3.0F).setUnlocalizedName("Coal");
-    public static final Block Emerald = new ModOre(x.Emerald).registerTexture("Emerald").setHardness(3.0F).setUnlocalizedName("Emerald"); 
+    public static final Block Iron = new ModOre().registerTexture("Iron").setHardness(3.0F).setBlockName("Iron");
+    public static final Block Gold = new ModOre().registerTexture("Gold").setHardness(3.0F).setBlockName("Gold");
+    public static final Block Lapis = new ModOre().registerTexture("Lapis").setHardness(3.0F).setBlockName("Lapis");
+    public static final Block Redstone = new ModOre().registerTexture("Redstone").setHardness(3.0F).setBlockName("Redstone");
+    public static final Block Diamond = new ModOre().registerTexture("Diamond").setHardness(3.0F).setBlockName("Diamond");
+    public static final Block Coal = new ModOre().registerTexture("Coal").setHardness(3.0F).setBlockName("Coal");
+    public static final Block Emerald = new ModOre().registerTexture("Emerald").setHardness(3.0F).setBlockName("Emerald"); 
     
-    public static final Block PoweredFurnace = new BlockPoweredFurnace(x.id++, false);
-    public static final Block PoweredFurnaceOn = new BlockPoweredFurnace(x.id++, true);
+    public static final Block PoweredFurnace = new BlockPoweredFurnace(false);
+    public static final Block PoweredFurnaceOn = new BlockPoweredFurnace(true);
     
 	public static void addNames(){
 		OreDictionary.registerOre("oreCopper", CopperOre);
@@ -146,10 +146,4 @@ public class BlockHelper extends HelperHandler{
 		addBlock(FlyLightStone, "Flylight Stone");
 		addBlock(SlowSand, "Sink Sand");
 	}
-
-	public static void addName(String par1, String name)
-	{
-		LanguageRegistry.instance().addStringLocalization("tile." + par1 + ".name", name);
-	}
-
 }
