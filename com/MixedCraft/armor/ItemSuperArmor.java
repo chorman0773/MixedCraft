04affd73e8cc0ea08c0de2c6171a9eaad59148d1
TheSlayerMC <brock.kerley@hotmail.com>
2014-02-08 09:23:26 +1030

    Pushed

--- com/MixedCraft/armor/ItemSuperArmor.java
+++ com/MixedCraft/armor/ItemSuperArmor.java
@@ -1,22 +1,10 @@
 package com.MixedCraft.armor;
 
-
 import java.util.List;
 
-import com.MixedCraft.ItemHelper;
-import com.MixedCraft.helper.Utils;
-
-import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.DamageSource;
-import net.minecraft.world.World;
-import net.minecraftforge.common.ISpecialArmor;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -24,8 +12,8 @@ public class ItemSuperArmor extends ItemArmor{
 
 	private String[] description;
 
-	public ItemSuperArmor(int par1, EnumArmorMaterial par2EnumArmorMaterial, int par3, int par4) {
-		super(par1, par2EnumArmorMaterial, par3, par4);
+	public ItemSuperArmor(ArmorMaterial par2EnumArmorMaterial, int par3, int par4) {
+		super(par2EnumArmorMaterial, par3, par4);
 	}
 
 	public ItemSuperArmor setDescriptors(String[] descs) {
